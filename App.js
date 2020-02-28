cb8cb9b73280751e0d7054217a8bab65ff0c77c3
RJArkell <51424552+RJArkell@users.noreply.github.com>
2020-02-27 20:52:10 -0500

    Updated chat view

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { ScrollView, StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import Start from './Start';
-import Chat from './Chat';
+import React from "react";
+import { ScrollView, StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import KeyboardSpacer from "react-native-keyboard-spacer"
+import Start from "./Start";
+import Chat from "./Chat";
 
 const navigator = createStackNavigator({
   Start: { screen: Start },
--- Chat.js
+++ Chat.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat"
+import KeyboardSpacer from "react-native-keyboard-spacer"
 
 export default class Chat extends React.Component {
   constructor() {
     super();
+    this.state = {
+      messages: []
+    }
   }
 
   static navigationOptions = ({ navigation }) => {
@@ -12,10 +17,61 @@ export default class Chat extends React.Component {
     };
   };
 
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any"
+          },
+        },
+        {
+          _id: 2,
+          text: this.props.navigation.state.params.name + " has entered the chat",
+          createdAt: new Date(),
+          system: true,
+        },
+      ]
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "lightblue"
+          }
+        }}
+      />
+    )
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: this.props.navigation.state.params.color, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, backgroundColor: this.props.navigation.state.params.color, justifyContent: "center" }}>
         <Text>Start chatting!</Text>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === "android" ? <KeyboardSpacer /> : null}
       </View>
     )
   }
